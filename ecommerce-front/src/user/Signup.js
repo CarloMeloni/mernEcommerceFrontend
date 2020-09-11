7f957730449182478acdfb38820655c4fe0459df
carloMeloni <carlo.meloni@hotmail.it>
2020-09-11 15:25:27 +0200

    Environment variables

--- ecommerce-front/src/user/Signup.js
+++ ecommerce-front/src/user/Signup.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from '../core/Layout';
+import { API } from '../config';
 
 const Signup = () => {
     return (
         <Layout title="SIGNUP" description="Registration">
-            SIGNUP
+            {API}
         </Layout>
     )
 };
