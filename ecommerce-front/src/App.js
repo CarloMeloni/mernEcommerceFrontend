ed46bcfd09520eea8a20f496a26941522d191935
carloMeloni <carlo.meloni@hotmail.it>
2020-09-11 11:49:19 +0200

    Adding Routes

--- ecommerce-front/src/App.js
+++ ecommerce-front/src/App.js
@@ -1,39 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import Routes from './Routes';
 
 const App = () => {
-    const [news, setNews] = useState([]);
-    const [searchQuery, setSearchQuery] = useState('React');
-    const [url, setUrl] = useState(`http://hn.algolia.com/api/v1/search?query=react`);
-
-    const fetchNews = () => {
-        fetch(url)
-        .then(result => result.json())
-        .then(data => setNews(data.hits))
-        .catch(err => console.log(err))
-    };
-
-    useEffect(() => { fetchNews(); }, [ url ])
-    
-    const handleChange = (e) => {
-        e.preventDefault();
-        setSearchQuery(e.target.value);
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
-    };
-
     return (
-        <div>
-            <h2>NEWWWWWWWWWWWWS</h2>
-            <form onSubmit={handleSubmit}>
-                <input type="text" value={searchQuery} onChange={handleChange} />
-                <button>SEARCHHHHHH</button>
-            </form>
-            {news.map((n, idx) => (<p key={idx}>{n.title}</p>))}
-        </div>
+       <Routes />
     )
+    
 }
 
 export default App;
---
+++ ecommerce-front/src/core/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            HOMEEEEEEE
+        </div>
+    )
+};
+
+export default Home;
