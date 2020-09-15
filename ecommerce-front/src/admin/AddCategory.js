0fceb5a1e594448c4d3e8d94c8f6a208642616a2
carloMeloni <carlo.meloni@hotmail.it>
2020-09-15 16:37:00 +0200

    Making http requesto to create a category

--- ecommerce-front/src/admin/AddCategory.js
+++ ecommerce-front/src/admin/AddCategory.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth/index';
+import { ceateCategory, createCategory } from './apiAdmin';
 
 const AddCategory = () => {
     const [name, setName] = useState('');
@@ -22,24 +23,51 @@ const AddCategory = () => {
         setSuccess(false);
 
         //MAKE REQUEST TO THE API TO CREATE CATEGORY
-
+        createCategory( user._id, token, {name} )
+            .then(data => {
+                if(data.error) {
+                    setError(data.error);
+                } else {
+                    setError('');
+                    setSuccess(true);
+                }
+            })
     };
 
     const newCategoryForm = () => (
         <form onSubmit={clickSubmit}>
             <div className="form-group">
                 <label className="text-muted">Category Name</label>
-                <input type="text" className="form-control" onChange={handleChange} value={name} autoFocus />
+                <input type="text" className="form-control" onChange={handleChange} value={name} autoFocus required />
             </div>
             <button className="btn btn-outline-primary">Create Category</button>
         </form>
     );
 
+    const showSuccess = () => {
+        if(success) {
+            return (<h3 className="text-success">{name} is Created</h3>);
+        }
+    };
+
+    const showError = () => {
+        if(error) {
+            return (<h3 className="text-danger">{' '}Category should be unique</h3>);
+        }
+    };
+    
+    const goBack = () => (
+        <div className="mt-5"><Link to="/admin/dashboard" className="text-warning">Back To Dashboard</Link></div>
+    );
+
     return (
-        <Layout title="Add a New Category" description={`Hey ${name}, add a new category here`}>
+        <Layout title="Add a New Category" description={`Hey ${user.name}, add a new category here`}>
             <div className="row">
                 <div className="col-md-8 offset-md2">
+                    {showSuccess()}
+                    {showError()}
                     {newCategoryForm()}
+                    {goBack()}
                 </div>
             </div>
         </Layout>
