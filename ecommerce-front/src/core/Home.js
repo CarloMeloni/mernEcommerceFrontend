9640fb79b96a9d72bb28dd49282d9e23adb493f0
carloMeloni <carlo.meloni@hotmail.it>
2020-09-16 15:57:08 +0200

    GET Products By Sell and Arrival

--- ecommerce-front/src/core/Home.js
+++ ecommerce-front/src/core/Home.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
+import { getProducts } from './apiCore';
 
 const Home = () => {
+    const [ productsBySell, setProductBySell ] = useState([]);
+    const [ productsByArrival, setProductByArrival ] = useState([]);
+    const [ error, setError ] = useState(false);
+
+    const loadProductsBySell = () => {
+        getProducts('sold')
+            .then(data => {
+                if(data.error) {
+                    setError(data.error);
+                } else {
+                    setProductBySell(data);
+                }
+            })
+    };
+
+    const loadProductsByArrival = () => {
+        getProducts('createdAt')
+            .then(data => {
+                if(data.error) {
+                    setError(data.error);
+                } else {
+                    setProductByArrival(data);
+                }
+            })
+    };
+
+    useEffect(() => {
+        loadProductsByArrival();
+        loadProductsBySell();
+    }, []);
+
     return (
         <Layout title="HOME" description="NODE REACT E-COMMERCE WEB APPLICATION">
-            .
+            {JSON.stringify(productsByArrival)}
+            <hr/>
+            {JSON.stringify(productsBySell)}
         </Layout>
     )
 };
