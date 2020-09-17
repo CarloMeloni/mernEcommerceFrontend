47867d780af61cbe71425c668cc5d8fceada2081
carloMeloni <carlo.meloni@hotmail.it>
2020-09-17 15:52:04 +0200

    Handling Categories Toggle

--- ecommerce-front/src/core/Checkbox.js
+++ ecommerce-front/src/core/Checkbox.js
@@ -1,12 +1,28 @@
 import React, { useState, useEffect } from 'react'
 
 const Checkbox = ({ categories }) => {
-    return categories.map((categ, idx) => (
+    const [ checked, setChecked ] = useState([]);
+
+    const handleToggle = categ => () => {
+        const currentCategoryId = checked.indexOf(categ);
+        const newCheckedCategoryId = [...checked];
+
+        if(currentCategoryId === -1) {
+            newCheckedCategoryId.push(categ);
+        } else {
+            newCheckedCategoryId.splice(currentCategoryId, 1);
+        }
+
+        console.log(newCheckedCategoryId);
+        setChecked(newCheckedCategoryId);
+    };
+
+     return categories.map((categ, idx) => (
         <li key={idx} className="list-unstiled">
-            <input type="checkbox" className="form-check-input" />
+            <input onChange={handleToggle(categ._id)} value={checked.indexOf(categ._id === -1)} type="checkbox" className="form-check-input" />
             <label className="form-check-label">{categ.name}</label>
         </li>
     ))
 }
 
-export default Checkbox
+export default Checkbox;
