7881b90b4f4aaad6ae59117ddf41791e2d2911d9
carloMeloni <carlo.meloni@hotmail.it>
2020-09-17 12:24:55 +0200

    Rendering categories in the lest side bar in the Shop component

--- ecommerce-front/src/core/Shop.js
+++ ecommerce-front/src/core/Shop.js
@@ -1,12 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
 import Card from './Card';
+import { getCategories } from './apiCore';
 
 const Shop = () => {
+    const [ categories, setCategories ] = useState([]);
+    const [ error, setError ] = useState(false);
+
+     //LOAD CATEGORIES AND SET FORM DATA
+     const init = () => {
+        getCategories()
+            .then(data => {
+                if(data.error) {
+                    setError( data.error );
+                } else {
+                    setCategories( data );
+                }
+            })
+    };
+
+    useEffect(() => {
+        init();
+    }, [])
+
     return (
         <Layout title="SHOP" description="SEARCH AND FIND PRODUCT OF YOUR CHOICE">
             <div className="row">
-                <div className="col-4">left</div>
+                <div className="col-4">
+                    {JSON.stringify(categories)}
+                </div>
                 <div className="col-8">right</div>
             </div>
         </Layout>
