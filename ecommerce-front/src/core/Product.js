41766e021b931efcfbfb1c764b1c73191c5125fd
carloMeloni <carlo.meloni@hotmail.it>
2020-10-02 12:03:11 +0200

    Showing related products in the single product page

--- ecommerce-front/src/core/Product.js
+++ ecommerce-front/src/core/Product.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
-import { read } from './apiCore';
+import { read, listRelated } from './apiCore';
 import Card from './Card';
 import Search from './Search';
 
 const Product = (props) => {
     const [product, setProduct] = useState({});
+    const [relatedProduct, setRelatedProduct] = useState([]);
     const [error, setError] = useState(false);
 
     const loadSingleProduct = (productId) => {
@@ -15,6 +16,16 @@ const Product = (props) => {
                     setError(data.error);
                 } else {
                     setProduct(data);
+
+                    //FETCH RELATED PRODUCT
+                    listRelated(data._id)
+                        .then(data => {
+                            if(data.error) {
+                                setError(data.error);
+                            } else {
+                                setRelatedProduct(data);
+                            }
+                        })
                 }
             })
     };
@@ -22,12 +33,22 @@ const Product = (props) => {
     useEffect(() => {
         const productId = props.match.params.productId;
         loadSingleProduct(productId);
-    }, [])
+    }, [props])
 
     return (
         <Layout className="container-fluid" title={product && product.name} description={product && product.description && product.description.substring(0, 100)}>
             <div className="row">
-                {product && product.description && <Card product={product} showViewProductButton={false} />}
+                <div className="col-8">
+                    {product && product.description && <Card product={product} showViewProductButton={false} />}
+                </div>
+                <div className="col-4">
+                    <h4>Related Product</h4>
+                    {relatedProduct.map((prod, idx) => (
+                        <div className="mb-3">
+                            <Card key={idx} product={prod} />
+                        </div>
+                    ))}
+                </div>
             </div>
         </Layout>
     )
