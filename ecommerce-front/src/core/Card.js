6bda9e1f11ac12beeae220be3fc173c43d9c244a
carloMeloni <carlo.meloni@hotmail.it>
2020-10-02 15:54:42 +0200

    Start working with CART (Add item to the cart)

--- ecommerce-front/src/core/Card.js
+++ ecommerce-front/src/core/Card.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import ShowImage from './ShowImage'
 import moment from 'moment';
+import { addItem } from './cartHelpers';
 
 const Card = ({ product, showViewProductButton = true, showAddToCartButton = true }) => {
+    const [redirect, setRedirect] = useState(false);
+
     const showStock = quantity => {
         return quantity > 0 
             ? <span className="badge badge-primary badge-pill">In stock</span> 
             : <span className="badge badge-primary badge-pill">Out of stock</span> 
     }
 
+    const addToCart = () => {
+        addItem(product, () => {
+            setRedirect(true);
+        })
+    };
+
+    const shouldRedirect = (redirect) => {
+        if(redirect) {
+            return <Redirect to='/cart' />
+        }
+    };
+
     return (
             <div className="card">
                 <div className="card-header name">{product.name}</div>
                 <div className="card-body">
+                    {shouldRedirect(redirect)}
                     <ShowImage item={product} url="product" />
                     <p className="lead mt-2">{product.description.substring(0, 100)}</p>
                     <p className="black-10">${product.price}</p>
@@ -24,7 +40,7 @@ const Card = ({ product, showViewProductButton = true, showAddToCartButton = tru
                     <Link to={`/product/${product._id}`}>
                         {showViewProductButton && <button  className="btn btn-outline-primary mt-2 mb-2 mr-2">View Product</button>}
                     </Link>
-                    {showAddToCartButton && <button  className="btn btn-outline-warning mt-2 mb-2">Add To Cart</button>}
+                    {showAddToCartButton && <button onClick={addToCart} className="btn btn-outline-warning mt-2 mb-2">Add To Cart</button>}
                 </div>
             </div>
     )
---
+++ ecommerce-front/src/core/cartHelpers.js
@@ -0,0 +1,29 @@
+export const addItem = (item, next) => {
+    let cart = [];
+
+    if(typeof window !== 'undefined') {
+        if(localStorage.getItem('cart')) {
+            cart = JSON.parse(localStorage.getItem('cart')); // TO CONVERT JSON FILES INTO OBJECT, JSON.stringify the opposite
+        }
+
+        cart.push({
+            ...item,
+            count: 1
+        });
+
+        //REMOVE DUPLICATES
+        //BUILD AN ARRAY FORM NEW SET AND TURN IT BACK INTO ARRAY USING Array.from
+        //SO THAT LATER WE CAN RE-MAP IT
+        //NEW SET WILL ONLY ALLOW UNIQUE VALUES IN IT
+        //SO PASS THE IDS  OF EACH OBJECT/PRODUCT
+        //IF THE LOOP TRIES TO ADD THE SAME VALUE AGAIN, IT'LL GET IGNORED
+        //...WITH THE ARRAY OF IDS WE GOT ON WHEN FIRST map() WAS USED
+        //RUN map() ON IT AGAIN AND RETURN THE ACTUAL PRODUCT FROM THE CART
+        cart = Array.from(new Set(cart.map((p) => (p._id)))).map(id => {
+            return cart.find(p => p._id === id);
+        });
+
+        localStorage.setItem('cart', JSON.stringify(cart));
+        next();
+    }
+};
