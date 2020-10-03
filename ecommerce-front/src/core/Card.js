b0739f8b04d8cc38e4f5740f62e9d6acf1d06370
carloMeloni <carlo.meloni@hotmail.it>
2020-10-03 16:03:41 +0200

    Updating the product quantity in the cart

--- ecommerce-front/src/core/Card.js
+++ ecommerce-front/src/core/Card.js
@@ -2,16 +2,36 @@ import React, { useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import ShowImage from './ShowImage'
 import moment from 'moment';
-import { addItem } from './cartHelpers';
+import { addItem, updateItem } from './cartHelpers';
 
-const Card = ({ product, showViewProductButton = true, showAddToCartButton = true }) => {
+const Card = ({ product, showViewProductButton = true, showAddToCartButton = true, cartUpdate = false }) => {
     const [redirect, setRedirect] = useState(false);
+    const [count, setCount] = useState(product.count);
 
     const showStock = quantity => {
         return quantity > 0 
             ? <span className="badge badge-primary badge-pill">In stock</span> 
             : <span className="badge badge-primary badge-pill">Out of stock</span> 
+    };
+
+    const handleChange = id => event => {
+        setCount(event.target.value < 1 ? 1 : event.target.value);
+        if(event.target.value >= 1) {
+            updateItem(id, event.target.value)
+        }
     }
+
+    const showCartUpdateOptions = (cartUpdate) => {
+        return cartUpdate && 
+        <div>
+            <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                    <span className="input-group-text">Adjust quantity</span>    
+                </div>
+                <input type="number" className="form-control" value={count} onChange={handleChange(product._id)} />      
+            </div>    
+        </div>
+    };
 
     const addToCart = () => {
         addItem(product, () => {
@@ -41,6 +61,7 @@ const Card = ({ product, showViewProductButton = true, showAddToCartButton = tru
                         {showViewProductButton && <button  className="btn btn-outline-primary mt-2 mb-2 mr-2">View Product</button>}
                     </Link>
                     {showAddToCartButton && <button onClick={addToCart} className="btn btn-outline-warning mt-2 mb-2">Add To Cart</button>}
+                    {showCartUpdateOptions(cartUpdate)}
                 </div>
             </div>
     )
--- ecommerce-front/src/core/cartHelpers.js
+++ ecommerce-front/src/core/cartHelpers.js
@@ -47,3 +47,20 @@ export const getCart = () => {
 
     return [];
 }
+
+export const updateItem = (productId, count) => {
+    let cart = [];
+    if(typeof window !== 'undefined') {
+        if(localStorage.getItem('cart')) {
+            cart = JSON.parse(localStorage.getItem('cart'));
+        }
+
+        cart.map((prod, idx) => {
+            if(prod._id === productId) {
+                cart[idx].count = count;
+            }
+        });
+
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }    
+};
