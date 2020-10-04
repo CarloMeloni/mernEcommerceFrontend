e7ef866c109011361bb7f6b63bd7b5e687eeacd0
carloMeloni <carlo.meloni@hotmail.it>
2020-10-04 16:10:30 +0200

    Setting Up Braintree for payments

--- ecommerce-front/src/core/Checkout.js
+++ ecommerce-front/src/core/Checkout.js
@@ -1,12 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
-import { getProducts } from './apiCore';
+import { getProducts, getBraintreeClientToken } from './apiCore';
 import Card from './Card';
 import Search from './Search';
 import {isAuthenticated} from '../auth/index';
 import { Link } from 'react-router-dom';
+import DropIn from 'braintree-web-drop-in-react';
 
 const Checkout = ({ products, setRun = f => f, run = undefined }) => {
+    const [data, setData] = useState({
+        success: false,
+        clientToken: null,
+        error: '',
+        instance: {},
+        address: ''
+    });
+
+    const userId = isAuthenticated() && isAuthenticated().user._id;
+    const token = isAuthenticated() && isAuthenticated().token;
+
+    const getToken = (userId, token) => {
+        getBraintreeClientToken(userId, token)
+            .then(data => {
+                if(data.error) {
+                    setData({ ...data, error: data.error });
+                } else {
+                    setData({ ...data, clientToken: data.clientToken });
+                }
+            })
+    };
+
+    useEffect(() => {
+        getToken(userId, token);
+    }, []);
+
     const getTotal = () => {
         return products.reduce((currentValue, nextValue) => {
             return currentValue + nextValue.count * nextValue.price;
@@ -16,7 +43,7 @@ const Checkout = ({ products, setRun = f => f, run = undefined }) => {
     const showCheckout = () => {
         return isAuthenticated() 
         ? (
-            <button className="btn btn-success">Checkout</button>
+            <div>{showDropIn()}</div>
         ) 
         : (
             <Link to="/signin">
@@ -24,11 +51,28 @@ const Checkout = ({ products, setRun = f => f, run = undefined }) => {
             </Link>
         )};
 
+    const showDropIn = () => (
+        <div>
+            {data.clientToken !== null && products.length > 0 
+                ? (
+                    <div>
+                        <DropIn options={{
+                            authorization: data.clientToken
+                         }}
+                            onInstance={(instance) => data.instance = instance}
+                          />
+                        <button className="btn btn-success">Checkout</button>
+                    </div>
+                ) 
+                : null}
+        </div>
+    );
+
     return (
         <div>
            <h2>Total: £{getTotal()}</h2>
 
-           {showCheckout}
+           {showCheckout()}
         </div>
     )
 }
